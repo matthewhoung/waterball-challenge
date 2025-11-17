e8331df93d0a352f57734827c8d23531b2adeffd
matthewhoung <matthewhoung@gmail.com>
2025-11-17 16:32:35 +0800

    initialize Java project as backend and constructed the healthcheck API endpoint (close#4)

---
+++ backend/src/test/java/tw/waterballsa/challenge/WaterballChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.waterballsa.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterballChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
